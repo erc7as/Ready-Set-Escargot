0f34670fa3d412da0304639027d04b6cc601a4a4
jcapelle13 <jjc2bd@virginia.edu>
2016-02-27 15:04:00 -0500

    added an acceleration constant

--- Ready Set Escargot/Assets/Scripts/SnailScript.js
+++ Ready Set Escargot/Assets/Scripts/SnailScript.js
@@ -1,6 +1,6 @@
 ﻿#pragma strict
 var speed : int = 10;
-
+var acceleration : int = 1;
 
 
 function Start () {
@@ -15,22 +15,22 @@ function Update () {
 if (Input.GetKey(KeyCode.LeftArrow)) {
 	//GetComponent.<Rigidbody2D>().velocity.x = -speed;
     //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(-1,0));
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(-1,0)*acceleration);
 }
 if (Input.GetKey(KeyCode.RightArrow)) {
 	//GetComponent.<Rigidbody2D>().velocity.x = speed;
     //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(1,0));
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(1,0)*acceleration);
 }
 if (Input.GetKey(KeyCode.DownArrow)) {
 	//GetComponent.<Rigidbody2D>().velocity.y = -speed;
     //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,-1));
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,-1)*acceleration);
 }
 if (Input.GetKey(KeyCode.UpArrow)) {
 	//GetComponent.<Rigidbody2D>().velocity.y = speed;
     //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,1));
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,1)*acceleration);
 }
 
 
