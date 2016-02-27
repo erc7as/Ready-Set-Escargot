3033e0ac3f296718142ec83850572d0c48e0ca1e
jcapelle13 <jjc2bd@virginia.edu>
2016-02-27 14:48:27 -0500

    Added Walls, the snail has force and collisions

--- Ready Set Escargot/Assets/Scripts/SnailScript.js
+++ Ready Set Escargot/Assets/Scripts/SnailScript.js
@@ -6,7 +6,6 @@ var speed : int = 10;
 function Start () {
 
 
-
 }
 
 
@@ -14,20 +13,24 @@ function Start () {
 function Update () {
 
 if (Input.GetKey(KeyCode.LeftArrow)) {
-	GetComponent.<Rigidbody2D>().velocity.x = -speed;
-	transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
+	//GetComponent.<Rigidbody2D>().velocity.x = -speed;
+    //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(-1,0));
 }
 if (Input.GetKey(KeyCode.RightArrow)) {
-	GetComponent.<Rigidbody2D>().velocity.x = speed;
-	transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
+	//GetComponent.<Rigidbody2D>().velocity.x = speed;
+    //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(1,0));
 }
 if (Input.GetKey(KeyCode.DownArrow)) {
-	GetComponent.<Rigidbody2D>().velocity.y = -speed;
-	transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
+	//GetComponent.<Rigidbody2D>().velocity.y = -speed;
+    //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,-1));
 }
 if (Input.GetKey(KeyCode.UpArrow)) {
-	GetComponent.<Rigidbody2D>().velocity.y = speed;
-	transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
+	//GetComponent.<Rigidbody2D>().velocity.y = speed;
+    //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
+    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,1));
 }
 
 
