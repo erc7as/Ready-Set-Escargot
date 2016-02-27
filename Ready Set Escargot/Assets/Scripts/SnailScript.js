37f77474684ce5a534df27332645df7221de9835
erc7as <erc7as@virginia.edu>
2016-02-27 14:01:08 -0500

    Initial Commit
    Some base code and placeholder assets

---
+++ Ready Set Escargot/Assets/Scripts/SnailScript.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var speed : int = 10;
+
+
+
+function Start () {
+
+
+
+}
+
+
+
+function Update () {
+
+if (Input.GetKey(KeyCode.LeftArrow)) {
+	GetComponent.<Rigidbody2D>().velocity.x = -speed;
+	transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
+}
+if (Input.GetKey(KeyCode.RightArrow)) {
+	GetComponent.<Rigidbody2D>().velocity.x = speed;
+	transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
+}
+if (Input.GetKey(KeyCode.DownArrow)) {
+	GetComponent.<Rigidbody2D>().velocity.y = -speed;
+	transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
+}
+if (Input.GetKey(KeyCode.UpArrow)) {
+	GetComponent.<Rigidbody2D>().velocity.y = speed;
+	transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
+}
+
+
+
+
+}
