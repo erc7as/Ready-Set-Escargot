b87dbe8753bf5d9736537a31201ce83988cf515d
Salindar165 <klanax135@gmail.com>
2016-03-08 10:15:48 -0500

    Added powerups (don't work yet)
    Yeah, they don't work yet. I'm committing this now to keep things simple
    though. I also changed the color of the walls on the test level so
    they're not the same color as the snail

---
+++ Ready Set Escargot/Assets/Scripts/PowerupScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var icon : Sprite;
+var hitplayer = false;
+
+function Start () {
+	gameObject.GetComponent(SpriteRenderer).sprite = icon;
+}
+
+function Trigger(trig:Collider2D){
+	if(trig.tag =="player"){
+		//hitplayer = true;
+		Destroy(trig.gameObject);
+	}
+	else{
+	}
+}
+/*
+function OnCollision2D(coll:Collision2D){
+	if(hitplayer == true){
+		
+	}
+	else{
+	}	
+}
+*/
+function Update () {
+}
