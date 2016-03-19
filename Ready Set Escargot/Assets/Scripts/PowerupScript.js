faaa9bcba13b77e1521abf95331f915f5b163130
erc7as <erc7as@virginia.edu>
2016-03-19 14:36:17 -0400

    some base code for random powerup selection
    added statements to select between 4 different powerups, will add actual
    code for powerups

--- Ready Set Escargot/Assets/Scripts/PowerupScript.js
+++ Ready Set Escargot/Assets/Scripts/PowerupScript.js
@@ -8,7 +8,21 @@ function Start (){
 }
 
 function OnTriggerEnter2D(trig: Collider2D){
-	if(trig.tag == "powerup"){
+    if(trig.tag == "powerup"){
+
+        var random : int = Random.value * 4 + 1;
+        if (random == 1) {
+            print("1");
+        }
+        else if (random == 2) {
+            print("2");
+        }
+        else if (random == 3) {
+            print("3");
+        }
+        else if (random == 4) {
+            print("4");
+        }
 		//hitplayer = true;
 		Destroy(trig.gameObject);
 		print("Powerup Destroyed");
