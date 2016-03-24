1bc4ebbc4b9672e8992c14ecc2fd4e040baa92e4
jcapelle13 <jjc2bd@virginia.edu>
2016-03-24 19:15:29 -0400

    Made an actual track!
    Changed the way the acceleration works, (it's not noticeable.)

--- Ready Set Escargot/Assets/Scripts/SnailScript.js
+++ Ready Set Escargot/Assets/Scripts/SnailScript.js
@@ -6,6 +6,7 @@ var downButton = KeyCode.DownArrow;
 var upButton = KeyCode.UpArrow;
 var maxSpeed : float = 3.0;
 var acceleration : int = 5;
+var forceVector : Vector2 = Vector2(0,0);
 
 
 function Start () {
@@ -16,26 +17,26 @@ function Start () {
 
 
 function Update () {
-
+    forceVector = Vector2(0,0);
 if (Input.GetKey(leftButton)) {
 	//GetComponent.<Rigidbody2D>().velocity.x = -speed;
     //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(-1,0)*acceleration);
+    forceVector += Vector2(-1,0);
 }
 if (Input.GetKey(rightButton)) {
 	//GetComponent.<Rigidbody2D>().velocity.x = speed;
     //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(1,0)*acceleration);
+    forceVector += Vector2(1,0);
 }
 if (Input.GetKey(downButton)) {
 	//GetComponent.<Rigidbody2D>().velocity.y = -speed;
     //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,-1)*acceleration);
+    forceVector += Vector2(0,-1);
 }
 if (Input.GetKey(upButton)) {
 	//GetComponent.<Rigidbody2D>().velocity.y = speed;
     //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
-    GetComponent.<Rigidbody2D>().AddForce(Vector2(0,1)*acceleration);
+    forceVector += Vector2(0,1);
 }
 
 if(GetComponent.<Rigidbody2D>().velocity.magnitude >= maxSpeed){
@@ -51,5 +52,5 @@ if(GetComponent.<Rigidbody2D>().velocity.magnitude > 0){
     }
     transform.eulerAngles.z = rotateAngle;
 }
-
+    GetComponent.<Rigidbody2D>().AddForce(forceVector*acceleration);
 }
