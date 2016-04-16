1bde44b0b9f86a0768db7e74953a3fe633c62c83
Salindar165 <klanax135@gmail.com>
2016-04-16 11:06:06 -0400

    Load Level Menu
    Menu works when you click on the level. Will add other level buttons
    soon.

---
+++ Ready Set Escargot/Assets/Scripts/level_select.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class level_select : MonoBehaviour {
+	public bool level1;
+	public bool level2;
+	public bool ret_main;
+	public bool quit;
+		void OnMouseUp(){
+		if (level1 == true) {
+			Application.LoadLevel (2);
+		} else if (level2 == true) {
+			Application.LoadLevel (3);
+		} else if (ret_main == true) {
+			Application.LoadLevel (0);
+		} else if (quit == true){
+			Application.Quit();
+		}
+	}
+	void OnMouseEnter(){
+		Renderer.material.GetColor = Color.red;
+	}
+	void OnMouseExit(){
+		Renderer.material.GetColor = Color.white;
+	}
+}
