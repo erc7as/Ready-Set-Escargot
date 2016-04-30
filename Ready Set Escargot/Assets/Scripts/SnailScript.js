ec6496b0444ef2d7f3a69eb520d98ba4912f0bfe
jcapelle13 <jjc2bd@virginia.edu>
2016-04-30 15:35:18 -0400

    Added Handling and mass, plus relative controls work on keyboards
    It probably still needs some tweaking...

--- Ready Set Escargot/Assets/Scripts/SnailScript.js
+++ Ready Set Escargot/Assets/Scripts/SnailScript.js
@@ -4,9 +4,7 @@ var leftButton = KeyCode.LeftArrow;
 var rightButton = KeyCode.RightArrow;
 var downButton = KeyCode.DownArrow;
 var upButton = KeyCode.UpArrow;
-var toggleButton = KeyCode.Greater;
-var maxSpeed : float = 3.0;
-var acceleration : int = 5;
+var toggleButton = KeyCode.Period;
 var forceVector : Vector2 = Vector2(0,0);
 var poweredUp : boolean = false;
 var powerCounter : int = 0;
@@ -20,7 +18,10 @@ var onFinishLine : boolean = false;
 var numLaps : int = 2;
 var powerUpTime : float;
 var absoluteControl : boolean = true;
+var maxSpeed : float = 3.0;
+var acceleration : int = 5;
 var handling : int = 5;
+var mass : float = 1;
 
 function Start () {
 	//acceleration = 5;
@@ -30,43 +31,66 @@ function Start () {
 
 
 function Update () {
+    GetComponent.<Rigidbody2D>().mass = mass;
     var horizAxis : float = Input.GetAxis("Horizontal_"+player);
     var vertAxis : float = Input.GetAxis("Vertical_"+player);
     var horizAxis2 : float = Input.GetAxis("HorizRight_"+player);
     forceVector = Vector2(0,0);
     if(absoluteControl){
         forceVector = Vector2(horizAxis*acceleration,vertAxis*acceleration);
+        if (Input.GetKey(leftButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.x = -speed;
+            //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
+            forceVector += Vector2(-1,0);
+        }
+        if (Input.GetKey(rightButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.x = speed;
+            //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
+            forceVector += Vector2(1,0);
+        }
+        if (Input.GetKey(downButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.y = -speed;
+            //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
+            forceVector += Vector2(0,-1);
+        }
+        if (Input.GetKey(upButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.y = speed;
+            //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
+            forceVector += Vector2(0,1);
+        }
     }
     else{
         transform.eulerAngles.z -= horizAxis2*handling;
         forceVector = transform.eulerAngles.z * transform.up * vertAxis;
+        if (Input.GetKey(leftButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.x = -speed;
+            //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
+            transform.eulerAngles.z += handling;
+        }
+        if (Input.GetKey(rightButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.x = speed;
+            //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
+            transform.eulerAngles.z -= handling;
+        }
+        if (Input.GetKey(downButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.y = -speed;
+            //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
+            forceVector = transform.eulerAngles.z * transform.up*-1;
+        }
+        if (Input.GetKey(upButton)) {
+            //GetComponent.<Rigidbody2D>().velocity.y = speed;
+            //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
+            forceVector = transform.eulerAngles.z * transform.up;
+
+        }
         
     }
     if (Input.GetKeyDown(toggleButton) || Input.GetButtonDown("Back_"+player)){
         absoluteControl = !absoluteControl;
         print("Absolute Controls on player "+player+": " + absoluteControl);
     }
-if (Input.GetKey(leftButton)) {
-	//GetComponent.<Rigidbody2D>().velocity.x = -speed;
-    //transform.Translate(Vector2(-1, 0) * Time.deltaTime*speed);
-    forceVector += Vector2(-1,0);
-}
-if (Input.GetKey(rightButton)) {
-	//GetComponent.<Rigidbody2D>().velocity.x = speed;
-    //transform.Translate(Vector2(1, 0) * Time.deltaTime*speed);
-    forceVector += Vector2(1,0);
-}
-if (Input.GetKey(downButton)) {
-	//GetComponent.<Rigidbody2D>().velocity.y = -speed;
-    //transform.Translate(Vector2(0, -1) * Time.deltaTime*speed);
-    forceVector += Vector2(0,-1);
-}
-if (Input.GetKey(upButton)) {
-	//GetComponent.<Rigidbody2D>().velocity.y = speed;
-    //transform.Translate(Vector2(0, 1) * Time.deltaTime*speed);
-    forceVector += Vector2(0,1);
-}
-
+    
+    
 if(GetComponent.<Rigidbody2D>().velocity.magnitude >= maxSpeed){
 //	print("Over max speed!");
     GetComponent.<Rigidbody2D>().velocity = GetComponent.<Rigidbody2D>().velocity.normalized * maxSpeed;
@@ -81,6 +105,9 @@ if(GetComponent.<Rigidbody2D>().velocity.magnitude > 0 && !slimy && absoluteCont
     transform.eulerAngles.z = rotateAngle;
 }
 forceVector = forceVector.normalized;
+if(Vector2.Angle(GetComponent.<Rigidbody2D>().velocity,forceVector) > 90 && GetComponent.<Rigidbody2D>().velocity.magnitude > 0){
+    forceVector = forceVector*handling;
+}
 GetComponent.<Rigidbody2D>().AddForce(forceVector*acceleration);
 powerCounter += 1;
 if (powerUpTime >= 2){
